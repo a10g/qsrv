619ec364092f11813b25a16386cf0df8c0cb8a69
Andrew Boring <andrew@andrewboring.com>
2019-09-26 12:38:33 -0400

    rename function file

--- qsrv_func.py
+++ qsrv_func.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-quote_list = []
-quote_path = 'fortunes'
+# A simple script to parse a BSD fortune file format without the strfile(8) index.
+
+
 
 def parse_fortunes(fortune_file):
-
+    quote_list = []
     with open(fortune_file) as f:
         quote = ""
         for line in f:
@@ -17,6 +18,8 @@ def parse_fortunes(fortune_file):
     return quote_list
 
 if __name__ == "__main__":
+    quote_list = []
+    quote_path = 'fortunes'
     fortune_file = quote_path + '/quotes'
     parse_fortunes(fortune_file)
     print(quote_list)
