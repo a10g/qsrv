cb3ea3e66fa5c6f11c024eae02ca26b5c1c3b9ba
Andrew Boring <andrew@andrewboring.com>
2019-09-26 12:39:30 -0400

    read fortune list into mem, pass list to response object

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 
 
-from flask import Flask
+from flask import Flask, request, Response
+import qsrv_func as q
 
+# configs - move to config.py eventually
+fortune_path = 'fortunes/'
+fortune_file = 'quotes'
 
-
+all_fortunes = fortune_path + fortune_file
+quotes = q.parse_fortunes(all_fortunes)
 
 app = Flask(__name__)
 
@@ -17,12 +22,11 @@ def api():
 
 @app.route("/api/quote")
 def quote():
-        return "A random quote goes here."
+        return Response(quotes, mimetype='text/plain')
 
 @app.route("/api/quote/monarch")
 def quote_monarch():
         return "A Monarch quote"
-
 
 
 if __name__ == "__main__":
